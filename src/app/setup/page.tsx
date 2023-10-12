d1e37068abed5753b297b0d0b95f09995eb560de
Dragos Rotaru <7482137+dragosrotaru@users.noreply.github.com>
2023-10-12 15:47:35 -0400

    implementing authentication flow

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,3 +1,31 @@
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Setup() {
-  return <>hello world</>;
+  const [data, setData] = useState(null);
+  const searchParams = useSearchParams();
+  const installationId = searchParams.get("installation_id");
+
+  useEffect(() => {
+    // Use the fetch API to make an HTTP GET request to an external API
+    fetch("/api/repositories?installation_id=" + installationId)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>installation_id: {installationId}</div>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </>
+  );
 }
