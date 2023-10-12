075d7f92a69749c3f8a3ffcf34de938c703a9a9e
Dragos Rotaru <7482137+dragosrotaru@users.noreply.github.com>
2023-10-12 16:06:12 -0400

    changed how private key is managed

--- src/app/octokit/index.ts
+++ src/app/octokit/index.ts
@@ -1,7 +1,11 @@
+import fs from "fs";
 import { App } from "octokit";
 
+const pemFilePath = "savant-dev-ai.2023-10-12.private-key";
+const pemContent = fs.readFileSync(pemFilePath, "utf8");
+
 const appId = process.env["GITHUB_APP_ID"];
-const privateKey = process.env["GITHUB_PRIVATE_KEY"];
+const privateKey = pemContent;
 const webhookSecret = process.env["GITHUB_WEBHOOK_SECRET"];
 
 if (!appId || !privateKey || !webhookSecret) {
