2585f81b3146e0dc9a4f47d64f2f93c451ac9cb4
Dragos Rotaru <7482137+dragosrotaru@users.noreply.github.com>
2023-10-12 14:39:12 -0400

    initial commit

---
+++ src/interfaces.ts
@@ -0,0 +1,11 @@
+/* 
+
+Account
+
+
+marketplace_purchase
+Setup URL 
+ redirect the customer to https://github.com/login/oauth/authorize.
+provision a customer account 
+Cherub-Perfected-Roving8
+*/
